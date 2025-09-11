13153a72ff326e4c2f8bffe8cc0cc5cff578cb98
mspc-72 <rishabhsinghtiwari.mindiii@gmail.com>
2025-09-11 15:36:24 +0530

    This code is for auth issue on Guest flow

--- lib/controller/login_controller.dart
+++ lib/controller/login_controller.dart
@@ -85,10 +85,6 @@ class LoginController extends GetxController {
       // Check if user exists in Firestore with password
       final credentials = await FireStoreUtils.getUserPasswordByEmail(googleUser.email);
 
-      if(Constant.isGustUser){
-        await FireStoreUtils.deleteUser();
-      }
-
       if (credentials != null && credentials['password'] != null && credentials['password']!.isNotEmpty) {
         final password = credentials['password']!;
         debugPrint("User password (Google): $password");
@@ -153,15 +149,8 @@ class LoginController extends GetxController {
         appleUserEmail = appleCredential.email??"N/A";
         debugPrint("appleCredential.email : ${appleCredential.email}");
       }
-
       debugPrint("appleEmail : $appleUserEmail");
-
-      if(Constant.isGustUser){
-        await FireStoreUtils.deleteUser();
-      }
-
       final credentials = await FireStoreUtils.getUserPasswordByEmail(appleUserEmail);
-
       if (credentials != null && credentials['password'] != null && credentials['password']!.isNotEmpty) {
         final password = credentials['password']!;
         debugPrint("User password (Apple): $password");
@@ -376,9 +365,6 @@ class LoginController extends GetxController {
   signInWithEmailAndPassword() async {
     ShowToastDialog.showLoader("please_wait".tr);
     try {
-      if(Constant.isGustUser){
-        await FireStoreUtils.deleteUser();
-      }
       FirebaseAuth.instance.signInWithEmailAndPassword(email: emailController.value.text.trim(),
           password: passwordController.value.text.trim()).then((value) async {
           await FireStoreUtils.userExistOrNot(value.user!.uid).then((userExit) async {
